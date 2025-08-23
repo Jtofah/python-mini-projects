4a567c2fc35b69679eccb23faf887addd7a6672f
jtofah <tofahjulius@gmail.com>
2025-08-23 17:32:36 +0000

    Add number guessing game

---
+++ guess the number/README.md
@@ -0,0 +1,33 @@
+# Guess the number
+
+A simple Python game where players try to guess a random number between 1 and 20 in 3 attempts.
+
+⸻
+
+## About
+
+This is a simple Python program where players try to guess a random number between 1 and 20. The game gives players 3 chances and provides hints after each guess. It's one of my beginner projects while learning Python programming 
+and demonstrates concepts like loops, conditionals, random number generation, and user input handling.
+
+
+
+## Tech Stack
+	•	Language: Python 3.x
+	•	Libraries used: "random"
+
+⸻
+
+## Features
+	•	Random number generation (1-20)
+	•	Input validation
+	•	Limited attempts (3 chances)
+    •	Helpful feedback for each guess  
+
+
+## How to run
+1. Run the script: \`python guess_the_number.py\`
+2. Enter your guess when prompted
+3. You have 3 chances to guess the correct number
+4. The game will tell you if your guess is too high or too low
+
+## Future improvement
---
+++ guess the number/guess_the_number.py
@@ -0,0 +1,32 @@
+"""
+    A simple number guessing game where the player has 3 chances 
+    to guess a random number between 1 and 20.
+    """
+import random
+
+answer = random.randint(1,20)
+chances = 3
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 20. You have 3 chances to guess it!")
+
+while chances > 0:
+    guess = int(input("guess the number between 1 and 20: "))
+    # Check the guess against the answer
+    if guess < answer:
+        result = "Oops!  Your guess was too low." #return a hint  if the guess is low
+    elif guess > answer:
+        result = "Oops!  Your guess was too high." #return a hint  if the guess is high
+    elif guess == answer:
+        result = "Nice!  You guessed correctly!" #return a success message if the guess is correct
+        print(result)
+        break #exit the  loop if the guess is correct
+        
+    print(result)
+    chances -= 1
+    if chances == 0:
+        print(f"Sorry, you've run out of chances.\nThe correct answer was {answer}")
+    else:
+        print(f"You have {chances} attempts left")
+  
+  
